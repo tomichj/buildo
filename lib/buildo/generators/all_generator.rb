4831d33601f94d2343248617ed1eddff0a55fcf4
Justin Tomich <tomichj@gmail.com>
2018-08-11 19:46:02 -0700

    fixing generate calls in all_generator

--- lib/buildo/generators/all_generator.rb
+++ lib/buildo/generators/all_generator.rb
@@ -4,9 +4,9 @@ module Buildo
   class AllGenerator < Rails::Generators::Base
     def all
       generate 'buildo:app_helpers'
-      generate 'buildo:auth_generator'
-      generate 'buildo:scaffolding_generator'
-      generate 'buildo:users_services_generator'
+      generate 'buildo:auth'
+      generate 'buildo:scaffolding'
+      generate 'buildo:users_services'
     end
   end
 end
